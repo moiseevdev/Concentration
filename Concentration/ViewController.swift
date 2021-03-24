8aafd82774878d0e69271f8e6f97756b0842cb01
Moiseev Andrei <andreimoiseew2009@yandex.ru>
2021-03-24 23:59:25 +0300

    update the structure and ViewController

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        flipCard(widthEmoji: "👻", on: sender)
+    }
+    
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCount += 1
+        flipCard(widthEmoji: "👍", on: sender)
+    }
+    
+    
+    
+    func flipCard(widthEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = .orange
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = .white
+        }
+    }
+    
 }
 
